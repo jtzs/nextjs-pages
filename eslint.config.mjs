ebddad442432f5887891849cc2c7d5252215fe3b
joshua <joshuatan89@outlook.com>
2025-04-07 16:58:54 +0800

    upgraded

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
